814b3a3540ccda7938fdf822a7a522a4cbd9a465
Nikola Socec <nikola.socec@gmail.com>
2021-05-02 18:50:25 +0200

    Reverted bad change and updated .gitignore

--- include/Polynomial.h
+++ include/Polynomial.h
@@ -4,41 +4,41 @@
 
 class Polynomial
 {
-    private:
-        static const int size = 100;
-        static const int steps = 50;
-        constexpr static const double tolerance = 1e-9;
+private:
+    static const int size = 100;
+    static const int steps = 50;
+    constexpr static const double tolerance = 1e-9;
 
-        std::vector<double> coefficients;
+    std::vector<double> coefficients;
 
-        static void genDerivativeMatrix();
+    static void genDerivativeMatrix();
 
-        void setCoefficientsToZero();
+    void setCoefficientsToZero();
 
-    public:
-        Polynomial();
-        explicit Polynomial(std::vector<double> &coefficientList);
+public:
+    Polynomial();
+    explicit Polynomial(std::vector<double> &coefficientList);
 
-        int getLeadingCoefficientIndex();
-        std::vector<double> getCoefficients();
-        double getValueAt(double x);
-        void printPolynomial();
-        void printForGrapher();
+    int getLeadingCoefficientIndex();
+    std::vector<double> getCoefficients();
+    double getValueAt(double x);
+    void printPolynomial();
+    void printForGrapher();
 
-        void multiplyByConst(double multiplier);
-        void multiplyByXtoN(int n);
-        Polynomial takeDerivative();
+    void multiplyByConst(double multiplier);
+    void multiplyByXtoN(int n);
+    Polynomial takeDerivative();
 
-        static Polynomial addPolynomials(Polynomial &pol1, Polynomial &pol2);
-        static Polynomial multiplyPolynomials(Polynomial &pol1, Polynomial &pol2);
-        static double findNewtonZero(double firstGuess, Polynomial inputPolynomial);
-        static double findHouseholderZero(double firstGuess, Polynomial inputPolynomial);
-        std::vector<double> getPolynomialRealZeros(double lowerBound, double upperBound);
+    static Polynomial addPolynomials(Polynomial &pol1, Polynomial &pol2);
+    static Polynomial multiplyPolynomials(Polynomial &pol1, Polynomial &pol2);
+    static double findNewtonZero(double firstGuess, Polynomial inputPolynomial);
+    static double findHouseholderZero(double firstGuess, Polynomial inputPolynomial);
+    std::vector<double> getPolynomialRealZeros(double lowerBound, double upperBound);
 
-        static Polynomial genLegendrePolynomialN(int n, Polynomial legendreN_minus_1, Polynomial legendreN_minus_2);
-        static std::vector<Polynomial> getLegendreSequenceUpToN(int maxN);
-        static void genInternalLegendreSequence();
-        static void getLegendreParametersForN(int n, std::vector<double> &zeros, std::vector<double> &weights);
+    static Polynomial genLegendrePolynomialN(int n, Polynomial legendreN_minus_1, Polynomial legendreN_minus_2);
+    static std::vector<Polynomial> getLegendreSequenceUpToN(int maxN);
+    static void genInternalLegendreSequence();
+    static void getLegendreParametersForN(int n, std::vector<double> &zeros, std::vector<double> &weights);
 };
 
 #endif //GENERAL_COIL_PROGRAM_POLYNOMIAL_H

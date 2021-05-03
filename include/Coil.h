c7cd2c246641e97597ee2158653868a371608346
Davor Dobrota <davor.dobrota@gmail.com>
2021-05-03 22:57:13 +0200

    Renamed the old class Coil to OldCoil

---
+++ include/Coil.h
@@ -0,0 +1,14 @@
+//
+// Created by unit5 on 03/05/2021.
+//
+
+#ifndef GENERAL_COIL_PROGRAM_COIL_H
+#define GENERAL_COIL_PROGRAM_COIL_H
+
+
+class Coil {
+
+};
+
+
+#endif //GENERAL_COIL_PROGRAM_COIL_H
---
+++ src/Coil.cxx
@@ -0,0 +1,5 @@
+//
+// Created by unit5 on 03/05/2021.
+//
+
+#include "Coil.h"

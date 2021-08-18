9e3ce72d84dd38ea5ee1b9a7be1056441c0772ce
Davor Dobrota <davor.dobrota@gmail.com>
2021-08-18 19:51:48 +0200

    Cleared main

--- src/main.cxx
+++ src/main.cxx
@@ -9,18 +9,6 @@ extern thread_pool tp;
 int main()
 {
 
-//    testCoilMutualInductanceZAxisDifferentGeometries();
-//
-//    testCoilMutualInductanceGeneralThinCoilAndFilament();
-//    testCoilMutualInductanceGeneralThinCoilAndThinCoil();
-//    testCoilMutualInductanceGeneralPancakeAndPancake();
-//    testCoilMutualInductanceGeneralRectangularAndFilament();
-//
-//    testCoilSelfInductance();
-
-    testCoilPositionAndRotation();
-
-//    testPerformanceForComputeAll(PrecisionFactor(6.0), 50000, 10, 16);
 
     return 0;
 }

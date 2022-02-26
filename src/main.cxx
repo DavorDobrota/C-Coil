f9558e3f0bafd9900eeaebdbba980718fbb96d27
Davor Dobrota <davor.dobrota@gmail.com>
2022-02-26 12:55:45 +0100

    Initial commit

--- src/main.cxx
+++ src/main.cxx
@@ -8,6 +8,12 @@
 
 int main()
 {
+    Coil coil = Coil(0.1, 0.1, 0.1, 10000);
+
+    for (int i = 0; i <= 140; ++i)
+    {
+        printf("%.15g\n", coil.computeAndSetSelfInductance(PrecisionFactor(1.0 + i * 0.1 )));
+    }
 
     return 0;
 }

3c9f2ca4cb13e632f5173decf8ba868f3efe1f08
Davor Dobrota <davor.dobrota@gmail.com>
2022-05-16 14:53:10 +0200

    Problematic windows behaviour code

--- src/main.cxx
+++ src/main.cxx
@@ -13,21 +13,40 @@
 int main()
 {
 
-//    benchComputeAllFields(PrecisionFactor(9.0), 10'000, 2, 8);
-    benchComputeAllFieldsEveryCoilType(415'701, 8);
+    const int xDim = 2501;
+    const int yDim = 2501;
+    const double xSize = 0.25;
+    const double ySize = 0.25;
+    const double zPos = 0.0;
 
-//    benchMInductanceZAxisMTScaling(12);
-//    benchMInductanceGeneralMTScaling(16);
-//    benchForceZAxisMTScaling(12);
-//    benchForceGeneralMTScaling(16);
-//    benchSelfInductance();
-//
-//    Coil coil = Coil(0.1, 0.1, 0.1, 10000);
-//
-//    for (int i = 0; i <= 140; ++i)
-//    {
-//        printf("%.15g\n", coil.computeAndSetSelfInductance(PrecisionFactor(1.0 + i * 0.1 )));
-//    }
+    FILE *output = fopen("output.txt", "w");
+
+    Coil coil = Coil(0.03, 0.03, 0.12, 3600, 10, PrecisionFactor(1.0), 12);
+    coil.setPositionAndOrientation(vec3::CoordVector3(vec3::CARTESIAN, 0.0, 0.0, 0.0), M_PI_2, 0.0);
+
+    printf("1\n");
+    std::vector<vec3::CoordVector3> positions(xDim * yDim);
+    printf("1\n");
+    std::vector<double> fields(xDim * yDim);
+    printf("1\n");
+
+    for (int i = 0; i < yDim; ++i)
+        for (int j = 0; j < xDim; ++j)
+        {
+            double yPos = ySize / (yDim-1) * (i - (yDim-1) / 2);
+            double xPos = xSize / (xDim-1) * (j - (xDim-1) / 2);
+            positions[i * xDim + j] = vec3::CoordVector3(vec3::CARTESIAN, xPos, yPos, zPos);
+        }
+    printf("1\n");
+    fields = coil.computeAllBFieldAbs(positions, CPU_MT);
+    printf("3\n");
+    for (int i = 0; i < yDim; ++i)
+    {
+        for (int j = 0; j < xDim; ++j)
+            fprintf(output, "%.15g\t", fields[i*xDim + j]);
+
+        fprintf(output, "\n");
+    }
 
     return 0;
 }
